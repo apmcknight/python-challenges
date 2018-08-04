b0adcb8c0989c129f699dab119b60226d709afe0
Adam McKnight <adammcknight@me.com>
2018-08-03 20:45:27 -0400

    Created a MadLib style game using Python

---
+++ madLib.py
@@ -0,0 +1,9 @@
+# The goal of this lesson was to create a Python Mad Lib style game.
+
+saying = input("Enter a Random Saying:")
+animal = input("Enter an Animal:")
+crush = input("Enter the name of Your Crush:")
+
+print("The Kangraoo Says " + saying)
+print(animal + " go moo")
+print("I love " + crush + " too")
